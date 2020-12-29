8c1fcf585a2b794db2db34b1838ea08a44a001d3
Abhay <ap3919529@gmail.com>
2020-12-29 17:58:24 +0530

    refactor: change in docs

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 function rpsGame(yourChoice){
     console.log(yourChoice);
+
     var userChoice , computerChoice;
-    userChoice = yourChoice.id
+
+    userChoice = yourChoice.id;
+
     computerChoice = randomNoToChoice(selectRandom());
+
     console.log('Bot choice:', computerChoice);
-    results = winnerDecider(userChoice, botChoice);
+
+    results = winnerDecider(userChoice, computerChoice);
     console.log(results);
 
     
@@ -18,7 +23,7 @@ function selectRandom(){
 
 function randomNoToChoice(number){
 
-    return ['rock', 'paper', 'scissor'][number];
+    return ['rock', 'scissor', 'paper'][number];
 
 }
 
@@ -26,8 +31,8 @@ function winnerDecider(yourChoice, botChoice){
 
     var rpsDB = {
         'rock': {'scissor':1 , 'rock':0.5 , 'paper':0},
-        'paper': {'rock':1 , 'paper':0.5 , 'scissor':0},
-        'scisssor': {'paper':1, 'scissor':0.5, 'rock':0}
+        'scisssor': {'paper':1, 'scissor':0.5, 'rock':0},
+        'paper': {'rock':1 , 'paper':0.5 , 'scissor':0}
     };
 
     var yourScore = rpsDB[yourChoice][botChoice];
