dad712b8ba5b64ed5d0b023581131f94c3a46601
Abhay <ap3919529@gmail.com>
2020-12-29 16:56:19 +0530

    create datsbase of choice
    create databse of userchoice and what will be be choice of the bot in an object
    with key: userChoice
    value: computerChoice

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ function randomNoToChoice(number){
 }
 
 function winnerDecider(){
-    
+
+    var rpsDB = {
+        'rock': {'scissor':1 , 'rock':0.5 , 'paper':0},
+        'paper': {'rock':1 , 'paper':0.5 , 'scissor':0},
+        'scisssor': {'paper':1, 'scissor':0.5, 'rock':0}
+    }
 }
 
