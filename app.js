00c11c5854ce36d60030b04ecdbf3cf67e87c4db
Abhay <ap3919529@gmail.com>
2020-12-29 16:44:34 +0530

    docs: give functions to comuterChoice
    Give the two functions that give random choice and associate with what to var computerChoice

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 function rpsGame(yourChoice){
     console.log(yourChoice);
     var userChoice , computerChoice;
+    computerChoice = randomNoToChoice(selectRandom);
+    
 }
 
 
 function selectRandom(){
 
-    return Math.floor(Math.random() * 3)
+    return Math.floor(Math.random() * 3);
 }
 
 function randomNoToChoice(number){
 
-    return ['rock', 'paper', 'scissor'][number]
+    return ['rock', 'paper', 'scissor'][number];
 
 }
+
